fc89da9f0038218d7e7a629d08fb8e24ae44f895
Mudit Vijay <muditv@aptclouds.com>
2024-09-30 13:56:36 +0530

    updateOfStateManageOfHomePage

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Menu } from 'antd';
 
-const Menubar = ({ onSelect }) => {
+const Menubar = ({ onSelect,selectedItem }) => {
   const menuItems = [
     { key: 'Organisation Setup', label: 'Setup Home' },
     { key: 'Users', label: 'Users' },
     { key: 'Custom Tabs', label: 'Custom Tabs' },
   ]; 
-
+ 
   return (
     <Menu
       mode="inline"
       onClick={(e) => onSelect(e.key)}
+      selectedKeys={[selectedItem]}  // Set the selectedKeys prop
+
       style={{ height: '100%' }}
     >
       {menuItems.map((item) => (
