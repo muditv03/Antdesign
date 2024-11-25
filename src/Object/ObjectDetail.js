49b7cb5e44ca4b3823090baab9481cd3ab0fc5d4
Shivam <shivamm@aptclouds.com>
2024-11-25 12:51:17 +0530

    Commit for Search Layout Changes

--- src/Object/ObjectDetail.js
+++ src/Object/ObjectDetail.js
@@ -9,6 +9,7 @@ import { BASE_URL } from '../Components/Constant';
 import ApiService from '../Components/apiService'; 
 import CreateObjectDrawer from './CreateObjectDrawer';
 import CreateLayout from './CreateLayout';
+import SearchLayout from './SearchLayout';
 
 
 const { Title } = Typography;
@@ -257,94 +258,10 @@ const ObjectFieldDetail = () => {
             <CreateLayout object={record} />
           </Card>
         </TabPane>
-        <TabPane tab="Search Result Layout" key="5">
-            <div style={{ display: 'flex', gap: '20px', alignItems: 'flex-start' }}>
-          {/* Available Fields */}
-          <div style={styles.listContainer}>
-            <h3>Available Fields</h3>
-            {loading ? (
-              <p>Loading...</p>
-            ) : (
-              availableFields.map((field) => (
-                <div
-                  key={field.name}
-                  style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    cursor: 'pointer',
-                  }}
-                  onClick={() =>
-                    toggleSelection(field, availableFields, setAvailableFields)
-                  }
-                >
-                  <input
-                    type="checkbox"
-                    checked={(field.isSelected && isEditMode) ? true : false}
-                    onChange={() =>
-                      toggleSelection(field, availableFields, setAvailableFields)
-                    }
-                  />
-                  <span style={{ marginLeft: '10px' }}>{field.name}</span>
-                </div>
-              ))
-            )}
-          </div>
-
-          {/* Arrow Buttons */}
-          <div style={{marginTop:'50px', marginLeft:'70px', width: '80px', display: 'flex', flexDirection: 'column', gap: '20px' }}>
-            <Button onClick={handleAddToSelected} 
-            disabled={!availableFields.length || !isEditMode}>
-              →
-            </Button>
-            <Button
-              onClick={handleRemoveFromSelected}
-              disabled={!selectedFields.length || !isEditMode}
-            >
-              ←
-            </Button>
-          </div>
-
-          {/* Selected Fields */}
-          <div style={styles.listContainer}>
-            <h3>Selected Fields</h3>
-            {selectedFields.map((field) => (
-              <div
-                key={field.name}
-                style={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  cursor: 'pointer',
-                }}
-                onClick={() =>
-                  toggleSelection(field, selectedFields, setSelectedFields)
-                }
-              >
-                <input
-                  type="checkbox"
-                  checked={(field.isSelected && isEditMode) ? true : false}
-                  onChange={() =>
-                    toggleSelection(field, selectedFields, setSelectedFields)
-                  }
-                />
-                <span style={{ marginLeft: '10px' }}>{field.name}</span>
-              </div>
-            ))}
-          </div>
-
-          {/* Save Button */}
-          <div style = {{marginTop: '60px'}}>
-              {isEditMode ? (
-              <Button type="primary" onClick={handleSave}>
-                Save
-              </Button>
-            ) : (
-              <Button type="default" onClick={handleEdit}>
-                Edit
-              </Button>
-            )}                  
-          </div>
-        </div>
-
+        <TabPane tab="Search Result Layout" key="6">
+          <Card>
+            <SearchLayout setAvailableFields={setAvailableFields}   setSelectedFields={setSelectedFields} availableFields={availableFields} selectedFields ={selectedFields} loading= {loading} isEditMode={isEditMode} toggleSelection={toggleSelection} handleAddToSelected={handleAddToSelected} handleRemoveFromSelected={handleRemoveFromSelected} handleSave={handleSave} handleEdit={handleEdit} />
+        </Card>
         </TabPane>
       </Tabs>
     </div>
